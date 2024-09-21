93c80a25dd57983f134531b4b1641a4e5a8e2c70
Gaspar <gaspar.tan1189@gmail.com>
2024-09-21 13:17:04 -0700

    attempt at getting fetch request

--- src/app/loggedin/page.js
+++ src/app/loggedin/page.js
@@ -1,14 +1,48 @@
 'use client';
 import { useSearchParams } from "next/navigation"
+let client_id = 'd281a48fc615470bae01e8658c953561'
+let client_secret = 'f885485f72094ea9a79b6c223d1cfb70'
+let redirect_uri = 'http://localhost:3000/callback'
 
 
 
 export default function Home() {
-    const searchParams = useSearchParams();
-    const code = searchParams.get('code')
+  // retrieves auth code from page.js
+  const searchParams = useSearchParams();
+  const code = searchParams.get('code')
+
+
+  var querystring = require('querystring');
+
+  // const Authorization = 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+
+  const url = 'https://accounts.spotify.com/api/token';
+
+  const body = {
+    grant_type: 'client_credentials',
+    code: code,
+    redirect_uri: redirect_uri
+  }
+
+  const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded', 
+    'Authorization' : 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+  }
+
+  fetch(url, {
+    method: 'POST',
+    headers: headers,
+    body: body
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.log(error
+  ))
+
   return (
     <div>
-      <h1>{code}</h1>
+      {/* <h1>{code}</h1> */}
+      <h1 className="text-white"> test </h1>
     </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
-let client_id = '665666f3816445e0b7dfcbdec816e62d'
+let client_id = 'd281a48fc615470bae01e8658c953561'
 let redirect_uri = 'http://localhost:3000/loggedin'
 var querystring = require('querystring')
 
@@ -10,7 +10,7 @@ var querystring = require('querystring')
 export default function Home() {
   const generateLink = () => {
 
-      var scopes = 'user-top-read ';
+      var scopes = 'user-top-read user-read-recently-played';
 
       return (
         <Link href = {'https://accounts.spotify.com/authorize?' + querystring.stringify( {
