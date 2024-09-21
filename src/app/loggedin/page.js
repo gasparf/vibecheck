729e9273ab1cc78fcae18cdb0663d7d124c1aad6
Gaspar <gaspar.tan1189@gmail.com>
2024-09-21 16:19:49 -0700

    fxied merge

--- src/app/loggedin/page.js
+++ src/app/loggedin/page.js
@@ -10,43 +10,6 @@ let client_secret = process.env.SPOTIFY_CLIENT_SECRET
 let redirect_uri = 'http://localhost:3000/loggedin'
 
 
-
-export default function Home() {
-  // retrieves auth code from page.js
-  const searchParams = useSearchParams();
-  const code = searchParams.get('code')
-
-  // we have to query string the data to send the request
-  var querystring = require('querystring');
-
-  // url to send the request to  
-  const url = 'https://accounts.spotify.com/api/token';
-
-  // body of the request
-  const body = {
-    grant_type: 'authorization_code',
-    code: code,
-    redirect_uri: redirect_uri,
-    client_id: client_id,
-    client_secret: client_secret,
-  }
-
-  // headers of the request for authorization
-  const headers = {
-    'Content-Type': 'application/x-www-form-urlencoded', 
-    'Authorization' : 'Basic ' + (new Buffer.from(`${client_id}:${client_secret}`).toString('base64'))
-  }
-
-  fetch(url, {
-    method: 'POST',
-    headers: headers,
-    body: body
-  })
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.log(error
-  ))
-
 export default async function Home() {
   // retrieves auth code from page.js
   const searchParams = useSearchParams();
