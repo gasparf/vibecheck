ed91256b6f6226ec715944ce7af6c7748bd8bf83
Gaspar <gaspar.tan1189@gmail.com>
2024-09-21 22:04:51 -0700

    trying to feel like im getting somewhere

--- src/app/loggedin/page.js
+++ src/app/loggedin/page.js
@@ -1,9 +1,9 @@
+'use client';
 import axios from 'axios'
 
 let client_id = process.env.SPOTIFY_CLIENT_ID
 let client_secret = process.env.SPOTIFY_CLIENT_SECRET
 let redirect_uri = 'http://localhost:3000/loggedin'
-
 
 export default async function Home({params,searchParams}) {
   // retrieves auth code from page.js
@@ -26,7 +26,7 @@ export default async function Home({params,searchParams}) {
   const headers = {
     'content-yype': 'application/x-www-form-urlencoded', 
     'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
-}
+  }
 
   // request function
   let response;
@@ -51,11 +51,11 @@ export default async function Home({params,searchParams}) {
       );
     }
   }
-    const auth_header = {
-        Authorization: 'Bearer ' + response.data.access_token
-    }
+  const auth_header = {
+      Authorization: 'Bearer ' + response.data.access_token
+  }
     
-    const artists  = await axios.get("https://api.spotify.com/v1/me/top/artists?limit=5", {headers: auth_header});
+  const artists  = await axios.get("https://api.spotify.com/v1/me/top/artists?limit=5", {headers: auth_header});
     // console.log(artists.data)
     // in the form of
     // external_urls: [Object],
@@ -68,28 +68,28 @@ export default async function Home({params,searchParams}) {
     // popularity: 64,
     // type: 'artist',
     // uri: 'spotify:artist:6CY7WNJfd5uZclcS3WeEjx'
-    const displaySongs = (artists) => { }
+  const displaySongs = (artists) => { }
 
     // line below contains sample of displaying artist name
     // <h1 className="text-lg text-red-500"> {songs.data.items[0].name} </h1>
 
 
     // obtain top 5 songs to generate recommended song
-    const topSongs = await axios.get("https://api.spotify.com/v1/me/top/tracks?limit=5", {headers: auth_header});
+  const topSongs = await axios.get("https://api.spotify.com/v1/me/top/tracks?limit=5", {headers: auth_header});
     // console.log(topSongs.data)
     
     // put songs ids into an array 
-    const songIds = []
-    for (let i = 0; i < topSongs.data.items.length; i++) {
-      songIds.push(topSongs.data.items[i].id)
-    }
+  const songIds = []
+  for (let i = 0; i < topSongs.data.items.length; i++) {
+    songIds.push(topSongs.data.items[i].id)
+  }
     // console.log(songIds)
     // generate recommended song based on top 5 songs
-    const recommendedSong = await axios.get("https://api.spotify.com/v1/recommendations?limit=1&seed_tracks=" + songIds.join(), {headers: auth_header});
-    console.log(recommendedSong.data)
+  const recommendedSong = await axios.get("https://api.spotify.com/v1/recommendations?limit=1&seed_tracks=" + songIds.join(), {headers: auth_header});
+  console.log(recommendedSong.data)
 
     // method to obtain users display name
-    const user_data = await axios.get("https://api.spotify.com/v1/me", {headers: auth_header});
+  const user_data = await axios.get("https://api.spotify.com/v1/me", {headers: auth_header});
 
   return (
     <div>
@@ -104,7 +104,7 @@ export default async function Home({params,searchParams}) {
         {/* Top artists section */}
         <div className="text-white absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[0%] grid grid-cols-2 gap-3 max-w-s">
           <p className="p-3 rounded text-[2.5vw] text-sm"> {artists.data.items[0].name} </p>
-          <p className="p-3 rounded text-s</div>m text-[2.5vw]">Artist 2</p>
+          <p className="p-3 rounded text-sm text-[2.5vw]">Artist 2</p>
           <p className="p-3 rounded text-sm text-[2.5vw]">Artist 3</p>
           <p className="p-3 rounded text-sm text-[2.5vw]">Artist 4</p>
           <p className="p-3 rounded text-sm text-[2.5vw]">Artist 5</p>
